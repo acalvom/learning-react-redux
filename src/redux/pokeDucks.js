d12bfa0deb94170949517fbf0f78367bb2344c59
acalvom <andreac.3108@gmail.com>
2021-05-14 09:00:18 +0200

    Explain Store and Ducks

---
+++ src/redux/pokeDucks.js
@@ -0,0 +1,14 @@
+/**
+ * Son los reduxes
+ * Los Ducks almacenan todas las constantes, acciones, reducers... en un solo archivo.
+ * Toda la configuración se guarda en este archivo. El uso de Ducks no es más que una metodología.
+ * En este archivo se almacenan las acciones para consumir datos de un API, a diferencia de los
+ * ejemplos anteriores, que se hacía la petición al API directamente en el componente
+ * Los Ducks requieren 3 cosas: constantes, reducers y acciones
+ */
+
+// Constants -- Almacena los datos del API aceptados por los reducers
+
+// Reducers -- Acepta la lista de datos del API y los envía a las constantes o estados
+
+// Actions -- Llama al API
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+/**
+ * En este archivo se guardan todos los estado para que estén disponibles en la app.
+ * El store reúne todas las piezas y archivos creado en el Duck.
+ * Se configura una vez y luego simplemente se añaden pequeñas modificaciones.
+ */
