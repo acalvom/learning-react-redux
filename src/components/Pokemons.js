734dcd6953496da8aa311460b7d95a3ef63caa25
acalvom <andreac.3108@gmail.com>
2021-05-17 09:12:56 +0200

    Select the data from the API to manage it

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -4,20 +4,30 @@ import {getPokemonsAction} from '../redux/pokeDucks'
 
 /* Hooks de React Redux
  * useDispatch sirve para consumir la acción getPokemonsAction
- * useSelector sirve para leer el array con el estado inicial
+ * useSelector sirve para leer el array con el estado inicial. Devuelve toda la tienda
  */
 
 
 const Pokemons = () => {
     const dispatch = useDispatch();
+    const pokemons = useSelector(store => store.pokemons.array);
+
     return (
-        <div>
-            Pokemons List
-            {/*Estamos con Redux y por lo tanto no se puede meter la acción como tal sino que
+        <div className="container mw-3">
+            <h2>Pokemons List</h2>
+            <button onClick={() => dispatch(getPokemonsAction())}>Get Pokemons</button>
+            {/*Con Redux no se puede meter la acción como tal sino que
             tiene que pasar por el dispatch:
              NOK --> ()=>getPokemonsAction()
              OK --> ()=>dispatch(getPokemonsAction())   */}
-            <button onClick={() => dispatch(getPokemonsAction())}>Get Pokemons</button>
+            <hr/>
+            <ul>
+                {
+                    pokemons.map((item) =>
+                        <li key={item.id}> {item.name}</li>
+                    )
+                }
+            </ul>
         </div>
     );
 };
