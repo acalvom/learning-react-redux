e5f8ec15abd0cdacb930746cca252d75f4ece1ff
acalvom <andreac.3108@gmail.com>
2021-05-17 08:53:02 +0200

    Dispatch action to consume the API

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import generateStore from "./redux/store";
 /*
  * Generar la tienda
  * Añadir la tienda creada como prop del Provider
+ * En el navegador, abrir la pestaña de Redux -> State para ver el array de pokemons
  */
 
 function App() {
     const store = generateStore()
     return (
+        // Se envuelven los componentes en los Providers
         <Provider store={store}>
             <Pokemons/>
         </Provider>
--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import {getPokemonsAction} from '../redux/pokeDucks'
+
+/* Hooks de React Redux
+ * useDispatch sirve para consumir la acción getPokemonsAction
+ * useSelector sirve para leer el array con el estado inicial
+ */
+
 
 const Pokemons = () => {
+    const dispatch = useDispatch();
     return (
         <div>
             Pokemons List
+            {/*Estamos con Redux y por lo tanto no se puede meter la acción como tal sino que
+            tiene que pasar por el dispatch:
+             NOK --> ()=>getPokemonsAction()
+             OK --> ()=>dispatch(getPokemonsAction())   */}
+            <button onClick={() => dispatch(getPokemonsAction())}>Get Pokemons</button>
         </div>
     );
 };
